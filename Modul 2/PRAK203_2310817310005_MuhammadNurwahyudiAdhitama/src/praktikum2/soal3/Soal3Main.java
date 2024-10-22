12ab1bbee00d888d82cf264083fe3372c4f6c1fd
Nurwahyudi Adhitama <144791435+Octazor@users.noreply.github.com>
2024-10-22 19:38:16 +0800

    Update Soal3Main.java

--- Modul 2/PRAK203_2310817310005_MuhammadNurwahyudiAdhitama/src/praktikum2/soal3/Soal3Main.java
+++ Modul 2/PRAK203_2310817310005_MuhammadNurwahyudiAdhitama/src/praktikum2/soal3/Soal3Main.java
@@ -3,15 +3,16 @@ package praktikum2.soal3;
 public class Soal3Main { 
     public static void main(String[] args) { 
         Pegawai p1 = new Pegawai(); 
-        //Tidak menggunakna tanda titik koma ";" 
+        //Tidak menggunakan tanda titik koma ";" 
         //p1.nama = "Roi" 
         p1.nama = "Roi";
         p1.asal = "Kingdom of Orvel"; 
         p1.setJabatan("Assasin"); 
         //Tidak menambahkan nilai atribut umur
         p1.umur = 17;
-       
-        System.out.println("Nama Pegawai: " + p1.getNama()); 
+        //Kesalahan pada "Nama Pegawai" karena di output cuma meminta "Nama"
+       // System.out.println("Nama Pegawai: " + p1.getNama()); 
+        System.out.println("Nama: " + p1.getNama()); 
         System.out.println("Asal: " + p1.getAsal()); 
 		System.out.println("Jabatan: " + p1.jabatan); 
 		//Tidak menambahkan kata tahun pada output
